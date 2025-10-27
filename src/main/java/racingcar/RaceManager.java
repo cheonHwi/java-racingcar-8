a5cda1a46bceb622787bfefe0a78e37df2f8f3da
cheonHwi <hoba0409@naver.com>
2025-10-27 22:48:52 +0900

    feat(RaceManager) 우승자 선정 함수 구현

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -1,15 +1,33 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RaceManager {
-    public void startRace(int moveTryCnt, List<Car> carList) {
-        for(int i = 0; i < moveTryCnt; i++) {
+    public void startRace(int moveTryCount, List<Car> carList) {
+        for(int i = 0; i < moveTryCount; i++) {
             for(Car car : carList) {
                 car.move();
                 car.printDistance();
             }
             System.out.println();
         }
+    }
+    public List<Car> announceWinner(List<Car> carList) {
+        List<Car> winner = new ArrayList<>();
+        int winnerPosition = -1;
+
+        for(Car car : carList) {
+            int position = car.getPosition();
+
+            if(position > winnerPosition) {
+                winnerPosition = position;
+                winner.clear();
+                winner.add(car);
+            }else if(position == winnerPosition) {
+                winner.add(car);
+            }
+        }
+        return winner;
     }
 }

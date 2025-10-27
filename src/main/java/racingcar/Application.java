40344fb07159b5d9f92b2d12becbba58c76b3a86
cheonHwi <hoba0409@naver.com>
2025-10-27 22:13:46 +0900

    feat(Application): 자동차, 이동시도횟수 입력부 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,22 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringValidator validator = new StringValidator();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = validator.validateIsEmpty(
+                InputConsole.readLine()
+        );
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int caseCnt = validator.validateRepeatCnt(
+                InputConsole.readLine()
+        );
+
+        String[] tokenizedCarList = input.split(",");
+
+        for(String token : tokenizedCarList) {
+            validator.validateCarName(token);
+        }
     }
 }

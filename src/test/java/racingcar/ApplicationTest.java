35356d2c1b95e30bd3e526f73859947e634a5b69
cheonHwi <hoba0409@naver.com>
2025-10-27 22:16:15 +0900

    test(ApplicationTest): 자동차, 이동시도횟수 테스트 구현
    - 빈 입력 검증
    - 요구사항에 부합하지 않는 입력은 잘못된 입력으로 판단

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,67 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 입력_테스트() {
+        assertSimpleTest(() -> {
+            run("pobi,woni,java", "3");
+        });
+    }
+
+    @Test
+    void 자동차_이름_입력_공백_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_6글자_이상_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_콤마로_입력_종료() {
+        assertSimpleTest(() -> {
+            run("pobi,woni,", "1");
+        });
+    }
+
+    @Test
+    void 자동차_이름_공백_이름_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java, ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_입력_공백_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", ""))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_숫자_제외_입력_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_숫자_입력_검증() {
+        assertSimpleTest(() -> {
+            run("pobi,woni,", "1");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});

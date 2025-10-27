112b85fdbc387c1d7f47fbbf45b4a9af27e4e9c8
cheonHwi <hoba0409@naver.com>
2025-10-27 22:49:19 +0900

    feat(Application) 경기 진행 루틴 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         StringValidator validator = new StringValidator();
+        RaceManager raceManager = new RaceManager();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = validator.validateIsEmpty(
@@ -15,9 +19,30 @@ public class Application {
         );
 
         String[] tokenizedCarList = input.split(",");
+        List<Car> carList = new ArrayList<>();
 
         for(String token : tokenizedCarList) {
-            validator.validateCarName(token);
+            carList.add(
+                    new Car(validator.validateCarName(token))
+            );
         }
+
+        raceManager.startRace(moveTryCount, carList);
+
+
+        List<Car> winnerList = raceManager.announceWinner(carList);
+
+        StringBuilder winnerString = new StringBuilder();
+        for(int winnerCnt = 0; winnerCnt < winnerList.size(); winnerCnt++) {
+            Car winner = winnerList.get(winnerCnt);
+
+            winnerString.append(winner.getName());
+            if(winnerCnt != winnerList.size()-1) {
+                winnerString.append(",");
+            }
+        }
+
+        System.out.println("최종 우승자 : " + winnerString);
+
     }
 }

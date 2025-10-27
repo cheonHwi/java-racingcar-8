428ad9b7990a6817adab9df826d71528c83c5bbf
cheonHwi <hoba0409@naver.com>
2025-10-27 22:57:41 +0900

    refactor(Application) 우승자 출력 문자열 구성 로직 수정
    - StringBuilder 기반에서 Stream 기반으로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -32,15 +33,9 @@ public class Application {
 
         List<Car> winnerList = raceManager.announceWinner(carList);
 
-        StringBuilder winnerString = new StringBuilder();
-        for(int winnerCnt = 0; winnerCnt < winnerList.size(); winnerCnt++) {
-            Car winner = winnerList.get(winnerCnt);
-
-            winnerString.append(winner.getName());
-            if(winnerCnt != winnerList.size()-1) {
-                winnerString.append(",");
-            }
-        }
+        String winnerString = winnerList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
 
         System.out.println("최종 우승자 : " + winnerString);
 
